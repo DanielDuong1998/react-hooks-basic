e9d045bb829b3e952521314ba28789241f15e379
DanielDuong1998 <dvkhangnt@gmail.com>
2020-08-04 12:34:17 +0700

    add todo into todolist and delete it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love you 1' },
+    { id: 2, title: 'I love you 2' },
+    { id: 3, title: 'I love you 3' },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log('form submit: ', formValues);
+    const newTodo = {
+      id: todoList[todoList.length - 1].id + 1,
+      ...formValues
+    }
+
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList)
+  }
+
   return (
     <div className="app">
-      <h1>Wellcome to reacj hook</h1>
+      <h1>React hook - to do list</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <TodoForm onSubmit={handleTodoFormSubmit} />
     </div>
   );
 }
